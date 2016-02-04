4a76b16c2f12039c14d7ea55df1ece4212420fad
veraliesim <soumi1rishav@gmail.com>
2016-02-04 19:25:57 +0530

    second commit

--- grampower_assignment/wsgi.py
+++ grampower_assignment/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grampower_assignment.settings")
 
 application = get_wsgi_application()
+try:
+	from dj_static import Cling
+	application = Cling(get_wsgi_application())
+except:
+	pass
